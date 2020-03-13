9954fc949c64ed7e0e5bc43db9aea5aad01bcf04
Narayan Pandey <npandey722@gmail.com>
2020-03-14 01:14:37 +0530

    setup test error resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+var webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 if (process.env.NODE_ENV == 'test') {
