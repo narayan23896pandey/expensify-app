90ff15daf2ef06f140720939b2af6a5c7450d08f
Narayan Pandey <npandey722@gmail.com>
2020-03-14 23:25:28 +0530

    satup test

--- src/tests/setUpTests.js
+++ src/tests/setUpTests.js
@@ -1,1 +1,7 @@
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 require('dotenv').config({ path: '.env.test' });
+Enzyme.configure({
+    adapter: new Adapter()
+});
