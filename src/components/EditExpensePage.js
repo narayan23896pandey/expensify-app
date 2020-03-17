ee5dffde43bfaa01bf1734d625a02f06428ac055
Narayan Pandey <npandey722@gmail.com>
2020-03-17 14:25:17 +0530

    Add style for Expense

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -15,11 +15,22 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm
-          expense={this.props.expense}
-          onSubmit={this.onSubmit}
-        />
-        <button onClick={this.onRemove}>Remove</button>
+        <div className="page-header">
+          <div className="content-container">
+            <div className="page-header__title"><h1>Edit Expense</h1></div>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm className="form"
+            expense={this.props.expense}
+            onSubmit={this.onSubmit}
+          />
+          <div>
+            <button class="button button--secondary" onClick={this.onRemove}>Remove Expense</button>
+          </div>
+        </div>
+
+
       </div>
     );
   }
